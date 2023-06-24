80589b3e0374cd7ac47d30834940b0c64ef03582
Jack Penson <fom@pop-os.localdomain>
2023-06-24 21:04:20 +0100

    completed rustlings

---
+++ clippy/target/debug/deps/clippy3-6f8daebc3b98bc7f.d
@@ -0,0 +1,8 @@
+/home/fom/code/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-6f8daebc3b98bc7f.rmeta: clippy3.rs Cargo.toml
+
+/home/fom/code/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-6f8daebc3b98bc7f.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ enums/enums1.rs
@@ -0,0 +1,17 @@
+// enums1.rs
+// No hints this time! ;)
+
+#[derive(Debug)]
+enum Message {
+    Quit(String),
+    Echo(String),
+    Move(String),
+    ChangeColor(String),
+}
+
+fn main() {
+    println!("{:?}", Message::Quit(String::from("Quitting")));
+    println!("{:?}", Message::Echo(String::from("Echo")));
+    println!("{:?}", Message::Move(String::from("Move")));
+    println!("{:?}", Message::ChangeColor(String::from("Color")));
+}
